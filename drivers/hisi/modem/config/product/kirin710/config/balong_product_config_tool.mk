b1bdb22789a409cf9c9667130f932e59bfce6da9
Supersonic <sithija@archlinux.localdomain>
2020-06-07 01:51:35 +0530

    add 9.1 branch

---
+++ drivers/hisi/modem/config/product/kirin710/config/balong_product_config_tool.mk
@@ -0,0 +1,13 @@
+# MD5: 7025ac21303a255908ed8a933d2d26d1
+CFG_PRODUCT_CONTROL_VERSION                 :="141"
+CFG_PRODUCT_HISILICON_VERSION               :=kirin710
+CFG_PRODUCT_DIFF_NV_FILTER               	      :=kirin710_miami
+CFG_PRODUCT_NV_BIN_MODEM_NUNBER             :=/bin_modem_number "3"
+CFG_PRODUCT_NV_XNV_PDT_VERSION              :=Kirin710
+CFG_PRODUCT_COMMON_NV_FILE_FORMAT			:=/common_nv_file_format "1"
+CFG_PRODUCT_XML_FOLDER_HIONE				:=1
+CFG_PRODUCT_NV_COMPRESS                        :=/compress_nv_file "1"
+CFG_PRODUCT_NV_PARSE_CUSTOMIZE			:=/parse_customize "1"
+CFG_PRODUCT_SPLIT_NV_BUILD                       := true
+CFG_PRODUCT_COMMON_NV_INHERIT_BY_PRODUCT		:= true
+CFG_PRODUCT_CUST_RESUME                         :=/support_cust_resume "1"
---
+++ drivers/hisi/modem/config/product/kirin710/config/product_config_tool.h
@@ -0,0 +1,53 @@
+/* MD5: 7025ac21303a255908ed8a933d2d26d1*/
+#if !defined(__PRODUCT_CONFIG_TOOL_H__)
+#define __PRODUCT_CONFIG_TOOL_H__
+
+#ifndef PRODUCT_CONTROL_VERSION
+#define PRODUCT_CONTROL_VERSION "141" 
+#endif 
+
+#ifndef PRODUCT_HISILICON_VERSION
+#define PRODUCT_HISILICON_VERSION kirin710 
+#endif 
+
+#ifndef PRODUCT_DIFF_NV_FILTER               	
+#define PRODUCT_DIFF_NV_FILTER               	 kirin710_miami 
+#endif 
+
+#ifndef PRODUCT_NV_BIN_MODEM_NUNBER
+#define PRODUCT_NV_BIN_MODEM_NUNBER bin_modem_number "3" 
+#endif 
+
+#ifndef PRODUCT_NV_XNV_PDT_VERSION
+#define PRODUCT_NV_XNV_PDT_VERSION Kirin710 
+#endif 
+
+#ifndef PRODUCT_COMMON_NV_FILE_FORMAT			
+#define PRODUCT_COMMON_NV_FILE_FORMAT			 common_nv_file_format "1" 
+#endif 
+
+#ifndef PRODUCT_XML_FOLDER_HIONE				
+#define PRODUCT_XML_FOLDER_HIONE				 1 
+#endif 
+
+#ifndef PRODUCT_NV_COMPRESS
+#define PRODUCT_NV_COMPRESS compress_nv_file "1" 
+#endif 
+
+#ifndef PRODUCT_NV_PARSE_CUSTOMIZE			
+#define PRODUCT_NV_PARSE_CUSTOMIZE			 parse_customize "1" 
+#endif 
+
+#ifndef PRODUCT_SPLIT_NV_BUILD
+#define PRODUCT_SPLIT_NV_BUILD true 
+#endif 
+
+#ifndef PRODUCT_COMMON_NV_INHERIT_BY_PRODUCT		
+#define PRODUCT_COMMON_NV_INHERIT_BY_PRODUCT		 true 
+#endif 
+
+#ifndef PRODUCT_CUST_RESUME
+#define PRODUCT_CUST_RESUME support_cust_resume "1" 
+#endif 
+
+#endif /*__PRODUCT_CONFIG_H__*/ 
